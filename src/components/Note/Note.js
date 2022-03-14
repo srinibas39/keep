0a766ed986a4bca85d6c0081bafde6b540d6de2d
srinibas <srinibaskhuntia39@gmail.com>
2022-03-15 03:35:11 +0530

    added colors to note

--- src/components/Note/Note.js
+++ src/components/Note/Note.js
@@ -1,15 +1,16 @@
 
+import { useFilter } from "../FilerContext";
 import { useNotes } from "../NoteContext"
 import "./Note.css"
 import { Snote } from "./Snote";
 
 export const Note = () => {
     const { notes, selectedFilter} = useNotes();
-
+     const {state}=useFilter()
     return <>
         {
             notes && notes.map((el, idx) => {
-                return <Snote  key={idx} el={el} selectedFilter={selectedFilter}/>
+                return <Snote  key={idx} el={el} selectedFilter={selectedFilter} color={state.color}/>
             })
         }
     </>
--- src/components/Note/Snote.js
+++ src/components/Note/Snote.js
@@ -2,14 +2,15 @@ import { useState } from "react"
 import { useFilter } from "../FilerContext";
 
 
-export const Snote = ({ el, selectedFilter }) => {
+export const Snote = ({ el, selectedFilter,color }) => {
 
     const [select, setSelect] = useState(selectedFilter);
     const { state } = useFilter();
-
+    const [col,setCol]=useState(color)
+     
     return <>
         {
-            state.filter === select && <div className="note">
+            state.filter === select && <div className={`note ${col}`}>
                 <h2>{select}</h2>
                 <p>{el}</p>
                 <button>
